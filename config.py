7e5e92a312fe13fa31cd1f4a3770048546527cc0
mage1711 <zizo17111998@gmail.com>
2021-07-26 11:21:52 +0200

    Changes in config file

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ MONTH = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "none"]
 CARDS_TARGET_PAGE = 0
 CARDS_START_PAGE = 0
-ITEMS_PER_PAGE = 5
+ITEMS_PER_PAGE = 3
 CARDS_URL_START = 'https://www.metacritic.com/browse/games/genre/metascore/action/all?view=condensed'
 JSON_FOLDER = 'jsonLists/'
